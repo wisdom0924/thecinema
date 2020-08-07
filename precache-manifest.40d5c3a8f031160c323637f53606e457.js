593789f1c7f081ccaf36a709795b6eaea1eca0b0
kooksk <0924wisdom@naver.com>
2020-08-07 15:45:27 +0900

    Updates

---
+++ precache-manifest.40d5c3a8f031160c323637f53606e457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f572d2cb5b5a105c152ec37faac9adb6",
+    "url": "/thecinema/index.html"
+  },
+  {
+    "revision": "ecfc254dd94fe14c8aa7",
+    "url": "/thecinema/static/css/main.5ed6de97.chunk.css"
+  },
+  {
+    "revision": "16e9ff92826d22296399",
+    "url": "/thecinema/static/js/2.13789b31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thecinema/static/js/2.13789b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfc254dd94fe14c8aa7",
+    "url": "/thecinema/static/js/main.09dd85df.chunk.js"
+  },
+  {
+    "revision": "50379eaded5e934733e1",
+    "url": "/thecinema/static/js/runtime-main.d48dd22b.js"
+  }
+]);
